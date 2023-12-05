294676de4384602fa74f00554e596b34ea46f9ab
printfxjy <756487027@qq.com>
2023-12-05 20:47:44 +0800

    分类管理业务开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -135,8 +135,6 @@ public class EmployeeController {
     }
 
 
-    @PutMapping
-    @ApiOperation("编辑员工信息")
     public Result updateEmployee(@RequestBody EmployeeDTO employeeDTO){
         log.info("编辑员工");
         employeeService.updateEmployee(employeeDTO);
